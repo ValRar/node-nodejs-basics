a43d4a805febf263061760c3635c942a96ef50c7
ValRar <val21219@gmail.com>
2023-06-16 19:28:50 +0300

    feat: solve hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import crypto from "node:crypto"
+import fs from "node:fs"
+
 const calculateHash = async () => {
-    // Write your code here 
+    const stream = fs.createReadStream("./src/hash/files/fileToCalculateHashFor.txt")
+    const hash = crypto.createHash("sha256")
+    stream.on("readable", () => {
+        const data = stream.read()
+        if (data) hash.update(data)
+        else console.log(hash.digest("hex"))
+    })
 };
 
 await calculateHash();
