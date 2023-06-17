188ab215918b7550f972acd86ee48d36488f1b10
ValRar <val21219@gmail.com>
2023-06-17 21:43:51 +0300

    feat: solve zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createGzip } from "node:zlib"
+import { pipeline } from "node:stream";
+import { createReadStream, createWriteStream } from "node:fs";
+import path from "node:path";
+
+const SOURCE_FILE = path.resolve("./src/zip/files/fileToCompress.txt")
+const DESTINATION_PATH = path.resolve("./src/zip/files/archive.gz")
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip()
+    const src = createReadStream(SOURCE_FILE)
+    const destination = createWriteStream(DESTINATION_PATH)
+    pipeline(src, gzip, destination, (err) => {
+        if (err) throw new Error("Error while compressing")
+    })
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,37 @@
+import { unzip } from "node:zlib"
+import { createReadStream, write, writeFileSync } from "node:fs";
+import path from "node:path";
+
+const DESTINATION_PATH = path.resolve("./src/zip/files/fileToCompress.txt")
+const SOURCE_FILE = path.resolve("./src/zip/files/archive.gz")
+
 const decompress = async () => {
-    // Write your code here 
+    const chunks = await readChunks(SOURCE_FILE)
+    const archiveBuffer = Buffer.concat(chunks)
+    unzip(archiveBuffer, (err, buffer) => {
+        if (err) throw new Error("Error while unzipping file")
+        // console.log(buffer.toString("utf-8"))
+        writeFileSync(DESTINATION_PATH, buffer.toString("utf-8"), (err) => {
+            if (err) throw new Error("Error while writing file")
+        })
+    })
 };
+
+const readChunks = (filePath) => {
+    const promise = new Promise((resolve, reject) => {
+        const archive = createReadStream(filePath)
+        const data = []
+        archive.on("data", (chunk) => {
+            data.push(chunk)
+        })
+        archive.on("end", () => {
+            resolve(data)
+        })
+        archive.on("error", (err) => {
+            reject(err)
+        })
+    })
+    return promise
+}
 
 await decompress();
